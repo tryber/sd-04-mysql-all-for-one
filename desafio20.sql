b2324d171c2c7a717f22f8db25dc284e6942f533
AliceAAtalla <aliceatalla36@gmail.com>
2020-08-19 17:54:43 -0300

    Desafio 25 a 27 - 3

--- desafio20.sql
+++ desafio20.sql
@@ -5,7 +5,5 @@ quantity,
 unit_price,
 discount,
 status_id,
-date_allocated,
-purchase_order_id,
 inventory_id)
-VALUES (69, 80, 15.0000, 15.0000, 0, 2, NULL, NULL, 129);
+VALUES (69, 80, 15.0000, 15.0000, 0, 2, 129);

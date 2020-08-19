7d46578c5ae9fe62a7a884f78af9c333118aeaba
Josias Viveiro <josiasviveiro@gmail.com>
2020-08-19 16:24:07 -0300

    desafio 7

--- desafio7.sql
+++ desafio7.sql
@@ -2,4 +2,4 @@
 7. Mostre apenas os ids dos 5 últimos registros da tabela products
  (a ordernação deve ser baseada na coluna id).
 */
-SELECT * FROM northwind.products ORDER BY id DESC LIMIT 5;
+SELECT id FROM northwind.products ORDER BY id DESC LIMIT 5;

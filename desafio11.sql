3388e8c1029c3d970d4e52cae5e71243210f6a5b
Leandro Marten <leandromarten@pop-os.localdomain>
2020-08-19 15:42:25 -0300

    Refatorado desafio 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,3 +1,5 @@
 -- Requirement 11
-select notes from northwind.purchase_orders
-where notes between 'Purchase generated based on Order #30' and 'Purchase generated based on Order #39';
+-- select notes from northwind.purchase_orders
+-- where notes between 'Purchase generated based on Order #30' and 'Purchase generated based on Order #39';
+select notes from purchase_orders
+where notes like '%#3_';

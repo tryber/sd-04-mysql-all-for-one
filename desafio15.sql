67e91ed89be814022fbc26ce1253c0ffb4f2319f
Thiago Tavares <thiagooo15@gmail.com>
2020-08-19 20:16:59 -0300

    broke my finger

--- desafio15.sql
+++ desafio15.sql
@@ -1,1 +1,3 @@
 SELECT HOUR(submitted_date) as submitted_date FROM northwind.purchase_orders;
+SELECT COUNT(*) AS orders_count FROM northwind.orders
+WHERE (employee_id IN (5,6)) AND shipper_id = 2;

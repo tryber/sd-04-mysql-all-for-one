e0e7f43fe07fa54a6c45b92c9579dbade1fb3809
Leandro Marten <leandromarten@pop-os.localdomain>
2020-08-19 14:46:31 -0300

    Requisito 12, 13 e 14

---
+++ desafio12.sql
@@ -0,0 +1,3 @@
+-- Requirement 12
+select submitted_date from northwind.purchase_orders
+where submitted_date like '2006-04-26%';
---
+++ desafio13.sql
@@ -0,0 +1,3 @@
+-- Requirement 13
+select supplier_id from northwind.purchase_orders
+where supplier_id = 1 or supplier_id = 3;

541b40a3b6f04bd4f6c874d17b47c878f342dd0c
Kaecio <kaecio@yahoo.com.br>
2020-08-20 00:55:48 -0300

    req12

--- desafio12.sql
+++ desafio12.sql
@@ -1,3 +1,4 @@
-select date(submitted_date) from purchase_orders where date(submitted_date) = "2006-04-26";
+select submitted_date from purchase_orders where date(submitted_date) = "2006-04-26";
+select submitted_date from purchase_orders;
 /*Mostre as submitted_date de purchase_orders 
 em que a submitted_date é do dia 26 de abril de 2006.*/

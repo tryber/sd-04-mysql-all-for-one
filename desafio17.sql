6631f5a65557c9cd58d569a9a771ba69317e2901
Kaecio <kaecio@yahoo.com.br>
2020-08-20 16:44:26 -0300

    req17

---
+++ desafio17.sql
@@ -0,0 +1,3 @@
+select id, supplier_id from purchase_orders where supplier_id = 1 or supplier_id = 3 or supplier_id = 5 or supplier_id = 7;
+/* Mostre os registros das colunas id e supplier_id das purchase_orders 
+em que os supplier_id sejam tanto 1, ou 3, ou 5, ou 7. */

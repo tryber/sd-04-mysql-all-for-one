894dcd923d0cdd1970bc257da39c8611e251f205
Kaecio <kaecio@yahoo.com.br>
2020-08-20 19:30:03 -0300

    req19

---
+++ desafio19.sql
@@ -0,0 +1,4 @@
+select employee_id, shipper_id as orders_count from orders where employee_id = 5 or employee_id = 6 and shipper_id = 2 ;
+
+/*Quanto, s pedidos foram feitos na tabela orders pelo employee_id 
+igual a 5 ou 6, e que foram enviados através do método shipper_id igual a 2? Chame a coluna de orders_count.*/

026d9646a318464f91ebb43748668c16f7fa37ea
leonardoinatel <leonardodamasceno@gec.inatel.br>
2020-08-20 16:32:31 -0300

    Bug fixed

--- desafio19.sql
+++ desafio19.sql
@@ -1,2 +1,1 @@
-#Quantos pedidos foram feitos na tabela orders pelo employee_id igual a 5 ou 6, e que foram enviados através do método shipper_id igual a 2? Chame a coluna de orders_count.select * from purchase_orders LIMIT 0, 1000
-select count(employee_id) from orders where employee_id in (5,6) and shipper_id = 2;
+select count(employee_id) as orders_count from orders where employee_id in (5,6) and shipper_id = 2;
--- desafio4.sql
+++ desafio4.sql
@@ -1,2 +1,1 @@
-#Conte quantos registros existem em product_name de products.
-select count(product_name) from products;
+select count(*) from products;

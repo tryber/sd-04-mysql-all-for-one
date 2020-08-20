c0595432c6697f637f586ea27e8587de52765456
Kaecio <kaecio@yahoo.com.br>
2020-08-20 01:26:39 -0300

    req12, colocando date depois do WHERE

--- desafio12.sql
+++ desafio12.sql
@@ -1,3 +1,3 @@
-select DATE(submitted_date) from purchase_orders where DATE(submitted_date) = "2006-04-26";
+select submitted_date from purchase_orders where date(submitted_date) = "2006-04-26";
 /*Mostre as submitted_date de purchase_orders 
 em que a submitted_date é do dia 26 de abril de 2006.*/

1765af725b459c07d8e401a0bb74ab4cd88298b0
Julio Cesar <stininmatic@hotmail.com>
2020-08-20 10:25:20 -0300

    vqv

--- desafio13.sql
+++ desafio13.sql
@@ -1,3 +1,4 @@
 SELECT supplier_id from northwind.purchase_orders
 WHERE supplier_id = 1
 OR supplier_id = 3;
+
--- desafio17.sql
+++ desafio17.sql
@@ -3,3 +3,4 @@ where supplier_id = 1
 or supplier_id = 3
 or supplier_id = 5
 or supplier_id = 7;
+

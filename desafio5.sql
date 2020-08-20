b2aace08033741b334990890036d467f88f245ef
Kaecio <kaecio@yahoo.com.br>
2020-08-20 00:00:44 -0300

    req5

--- desafio5.sql
+++ desafio5.sql
@@ -1,2 +1,4 @@
-select * from products;
 select * from products limit 13 offset 3;
+select * from products;
+/*Monte uma query que exiba os dados da tabela products a partir 
+do quarto registro até o décimo terceiro, incluindo tanto um quanto o outro*/

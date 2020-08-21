1950dce40065d6555703fea4e2980a89e28a25de
Kaecio <kaecio@yahoo.com.br>
2020-08-21 18:37:05 -0300

    req19

--- desafio19.sql
+++ desafio19.sql
@@ -1,6 +1,7 @@
-select shipper_id as orders_count from orders 
+select SUM(shipper_id) as orders_count from orders 
 where employee_id = 5 or employee_id = 6 
 and shipper_id = 2;
+select * from orders; 
 /*Quantos pedidos foram feitos na tabela orders 
 pelo employee_id igual a 5 ou 6,
  e que foram enviados através do método shipper_id igual a 2? 

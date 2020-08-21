678e55301b3b32ad6512e2d2d6d06e8234689ff1
Kaecio <kaecio@yahoo.com.br>
2020-08-21 15:13:21 -0300

    req25

--- desafio25.sql
+++ desafio25.sql
@@ -1,3 +1,3 @@
-select unit_price from order_details;
+select * from order_details;
 delete from order_details where unit_price < 10.0000;
 /*Delete todos os dados em que a unit_price da tabela order_details seja menor que 10.0000.*/

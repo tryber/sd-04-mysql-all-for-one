7db1e900d48ff5ddf74ea0b0c9f3086d3cb7a7f8
Kaecio <kaecio@yahoo.com.br>
2020-08-21 18:53:48 -0300

    req19, erro besta, agora vai

--- desafio19.sql
+++ desafio19.sql
@@ -1,7 +1,6 @@
 select COUNT(employee_id) as orders_count from orders 
 where employee_id = 5 or employee_id = 6 
 and shipper_id = 2;
-select * from orders; 
 /*Quantos pedidos foram feitos na tabela orders 
 pelo employee_id igual a 5 ou 6,
  e que foram enviados através do método shipper_id igual a 2? 

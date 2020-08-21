0998973a001d595cab682f59d2dfecd585131bee
Kaecio <kaecio@yahoo.com.br>
2020-08-21 18:43:50 -0300

    req19, ta osso

--- desafio19.sql
+++ desafio19.sql
@@ -1,4 +1,4 @@
-select SUM(shipper_id) as orders_count from orders 
+select SUM(employee_id) as orders_count from orders 
 where employee_id = 5 or employee_id = 6 
 and shipper_id = 2;
 select * from orders; 

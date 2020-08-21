cd81895d1eeaccbec6fc33e462cb4424c264ff72
leonardoinatel <leonardodamasceno@gec.inatel.br>
2020-08-21 14:06:45 -0300

    Done

--- desafio24.sql
+++ desafio24.sql
@@ -1,1 +1,1 @@
-update order_details set discount = 45 where unit_price < 10.0000 and id between 30 and 40;
+update order_details set discount = 45 where unit_price > 10.0000 and id between 30 and 40;

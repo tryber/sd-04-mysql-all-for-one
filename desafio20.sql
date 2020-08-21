d0a2472f315a2e794e30f62a9be9773c7e4c0adc
Kaecio <kaecio@yahoo.com.br>
2020-08-21 01:13:59 -0300

    req20

---
+++ desafio20.sql
@@ -0,0 +1,7 @@
+insert into order_details (order_id, product_id, quantity, unit_price, 
+discount, status_id, date_allocated, purchase_order_id, inventory_id)
+values(69,80,15.0000,15.0000,0,2,NUll,NULL,129);
+/* Adicione ao order_details uma linha com os seguintes dados: 
+order_id: 69, product_id: 80, quantity: 15.0000, unit_price: 15.0000, 
+discount: 0, status_id: 2, date_allocated: NULL, purchase_order_id: NULL 
+e inventory_id: 129. Obs.: o id deve ser incrementado automaticamente.*/

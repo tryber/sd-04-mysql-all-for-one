b2cb47157d66ab3efb15c62a2e9d471a00073039
Kaecio <kaecio@yahoo.com.br>
2020-08-21 18:28:41 -0300

    req19, esse me pegou mesmo ate no CC

--- desafio19.sql
+++ desafio19.sql
@@ -1,4 +1,7 @@
-select shipper_id as orders_count from orders where employee_id = 5 or employee_id = 6 and shipper_id = 2;
-/*Quantos pedidos foram feitos na tabela orders pelo employee_id igual a 5 ou 6,
- e que foram enviados através do método shipper_id igual a 2? Chame a coluna de orders_count *
- 
+select shipper_id as orders_count from orders 
+where employee_id = 5 or employee_id = 6 
+and shipper_id = 2;
+/*Quantos pedidos foram feitos na tabela orders 
+pelo employee_id igual a 5 ou 6,
+ e que foram enviados através do método shipper_id igual a 2? 
+ Chame a coluna de orders_count */

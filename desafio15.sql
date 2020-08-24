1d630b60fce76a6b743dfa8e61ba3f68453e5a9c
Thiago Tavares <thiagooo15@gmail.com>
2020-08-24 16:14:54 -0300

    meteoro home run

--- desafio15.sql
+++ desafio15.sql
@@ -1,3 +1,3 @@
-SELECT HOUR(submitted_date) as submitted_date FROM northwind.purchase_orders;
+SELECT HOUR(submitted_date) AS submitted_date FROM northwind.purchase_orders;
 SELECT COUNT(*) AS orders_count FROM northwind.orders
 WHERE (employee_id IN (5,6)) AND shipper_id = 2;

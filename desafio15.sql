0ae9bba9f31a0f7d37addf065688656b013265fe
Thiago Tavares <thiagooo15@gmail.com>
2020-08-24 16:28:22 -0300

    full crowling 100%

--- desafio15.sql
+++ desafio15.sql
@@ -1,3 +1,2 @@
-SELECT HOUR(submitted_date) AS submitted_date FROM northwind.purchase_orders;
-SELECT COUNT(*) AS orders_count FROM northwind.orders
-WHERE (employee_id IN (5,6)) AND shipper_id = 2;
+SELECT HOUR(submitted_date) AS submitted_date 
+FROM northwind.purchase_orders;
